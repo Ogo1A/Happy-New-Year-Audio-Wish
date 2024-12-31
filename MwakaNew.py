d86affb948bf8dce41d7b7327c30d0b51edf44a0
Darren Ogola <darren.sunday@strathmore.edu>
2024-12-31 20:55:24 +0300

    End  to MwakaNew.py

---
+++ MwakaNew.py
@@ -0,0 +1,15 @@
+import pyttsx3
+
+engine=pyttsx3.init()
+
+engine.say(
+    
+           
+           "Happy New Year 2025!"
+           " Wishing you a fantastic year ahead of you!"
+)
+print(
+    "Happy New Year 2025!\
+           Wishing you a fantastic year ahead of you!"
+)
+print.runAndWait
